baa1334eb71c9c0253d4839faeaecb0616b6cbd2
Zachary <zachary.15pierce@gmail.com>
2019-07-16 11:46:44 -0600

    Express now running

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(express.static(__dirname + '/../dist'));
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
