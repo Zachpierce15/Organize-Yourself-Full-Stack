8ba0587047913002c7ae379d94184b32b35b191b
Zachary <zachary.15pierce@gmail.com>
2019-07-17 14:54:24 -0600

    First component added to Project Starter

---
+++ client/src/components/app.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    //bind functions here
+  }
+  render() {
+    return (
+    <div>
+      You are rendering from app
+    </div>
+    )
+  }
+}
+export default App
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import App from './components/app.jsx'
 
 const title = 'React with Webpack and Babel';
 
-ReactDOM.render(
-  <div>{title}</div>,
-  document.getElementById('app')
+ReactDOM.render(<App />, document.getElementById('app')
 );
